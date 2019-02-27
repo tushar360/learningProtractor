69aede6851cfc5e04a41ac88ecc55290b053344f
tushar360 <47621456+tushar360@users.noreply.github.com>
2019-02-27 12:25:34 +0530

    First Commit
    Adding Configuration.js file

---
+++ configuration.js
@@ -0,0 +1,41 @@
+var baseDir=__dirname
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
+exports.config = {
+    directConnect: 'true',
+    specs: './src/main/specifications/*.js',
+	framework: 'jasmine2',
+	jasmineNodeOpts: {
+    showColors: true,
+        defaultTimeoutInterval: 360000,// Use colors in the command line report.
+  },
+  capabilities: {
+    browserName: 'chrome',
+	},
+  logLevel:"ERROR",
+  baseUrl:"http://juliemr.github.io/protractor-demo/",
+  onPrepare :  () =>{
+      jasmine.getEnv().addReporter(new SpecReporter({
+          spec: {
+              displayStacktrace: true
+          },
+          summary: {
+              displayDuration: true
+          }})),
+  browser.waitForAngularEnabled(false);
+  browser.get("");
+
+  },
+  onComplete: () =>{
+	  console.log('Complete')
+  },
+  onCleanUp: () =>{
+	  console.log('Clean Up')
+  },
+  afterLaunch: () =>{
+	  console.log('After launch')
+  },
+ params: {
+	 dir : baseDir
+ }
+};
